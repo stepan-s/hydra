7fe9dc778041e9dd0cc114fd4534835ae544de71
Stepan Strelets <s3lets@gmail.com>
2013-08-11 17:31:23 +0400

    Add example

---
+++ examples/RouterWithSensor/RouterWithSensor.cpp
@@ -0,0 +1,36 @@
+#include "hydra.h"
+#include "hydra_core.h"
+#include "hydra_lan.h"
+#include "hydra_nrf.h"
+#include "hydra_echo.h"
+#include "hydra_dht11.h"
+
+HydraLan lan = HydraLan();
+HydraNrf nrf = HydraNrf(9, 10);
+HydraCore core = HydraCore();
+HydraEcho echo = HydraEcho();
+HydraDht11 dht = HydraDht11(2);
+
+HydraComponentDescriptionList components = {
+	5, //total count
+	2, //netif count
+	3, //service count
+	(HydraComponentDescription[]) {
+		{& lan, HYDRA_SERVICE_NET},
+		{& nrf, HYDRA_SERVICE_NET},
+		{& core, HYDRA_SERVICE_CORE},
+		{& echo, HYDRA_SERVICE_ECHO},
+		{& dht, HYDRA_SERVICE_DHT11}
+	}
+};
+
+Hydra hydra = Hydra(& components);
+
+void setup() {
+	Serial.begin(115200);
+	hydra.init();
+}
+
+void loop() {
+	hydra.loop();
+}
